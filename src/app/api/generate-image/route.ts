4a66eddd32e88aae2a695fac9997937dc64c2ee3
Suryangsu Chandra <suryangsuchandra39@gmail.com>
2024-12-24 21:15:23 +0530

    Ready for deployment

--- src/app/api/generate-image/route.ts
+++ src/app/api/generate-image/route.ts
@@ -1,4 +1,7 @@
 import { NextResponse } from "next/server";
+import crypto from "crypto";
+import { put } from "@vercel/blob";
+
 
 export async function POST(request: Request) {
   try {
@@ -7,14 +10,56 @@ export async function POST(request: Request) {
 
     // TODO: Call your Image Generation API here
     // For now, we'll just echo back the text
+    console.log(text);
+
+    const apiSecret = request.headers.get("X-API-Key");
+
+    if (apiSecret !== process.env.API_KEY) {
+      return NextResponse.json(
+        { success: false, error: "Unauthorized" },
+        { status: 401 }
+      );
+    }
+    const url = new URL(process.env.Modal_URL || "");
+    url.searchParams.set("prompt", text);
+
+    console.log("Requesting URL",url.toString());
+
+    const response = await fetch(url.toString(), {
+      method: "GET",
+      headers: {
+        "X-API-Key": process.env.API_KEY || "",
+        Accept: "image/jpeg",
+      },
+    });
+
+    if (!response.ok) {
+      const errortext=await response.text();
+      console.error("API Response:",errortext);
+      throw new Error(
+        `HTTP error! status:${response.status},message:${errortext}`
+      );
+    }
+
+
+    const imageBuffer = await response.arrayBuffer();
+
+    const filename = `${crypto.randomUUID()}.jpg`
+
+    const blob=await put(filename,imageBuffer,{
+      access:"public",
+      contentType:"image/jpeg",
+    })
 
     return NextResponse.json({
-      success: true,
-      message: `Received: ${text}`,
+      success:true,
+      imageUrl:blob.url,
+      //store the prompt and imageurl in the database
     });
-  } catch (error) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
     return NextResponse.json(
-      { success: false, error: "Failed to process request" },
+      { success: false, error: error.message },
       { status: 500 }
     );
   }
